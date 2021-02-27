74fe5b764d708ae76e21dcb29fdd69542a8fc7ab
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-02-27 20:04:41 +0300

    feat: promises -> async/await

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require(`fs`);
+const fs = require(`fs`).promises;
 const path = require(`path`);
 const chalk = require(`chalk`);
 
@@ -85,23 +85,21 @@ const generateOffers = (count) => {
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
     if (countOffer > MAX_COUNT) {
-      console.error(`Не больше 1000 объявлений`);
+      console.error(chalk.red(`Не больше 1000 объявлений`));
       process.exit(ExitCode.uncaughtFatalException);
     }
 
     const content = JSON.stringify(generateOffers(countOffer));
-
-    fs.writeFile(FILE_NAME, content, (err) => {
-      if (err) {
-        return console.error(chalk.red(`Can't write data to file...`));
-      }
-
-      return console.info(chalk.green(`Operation success. File created.`));
-    });
+    try {
+      await fs.writeFile(FILE_NAME, content);
+      console.info(chalk.green(`Operation success. File created.`));
+    } catch (error) {
+      console.error(chalk.red(`Can't write data to file...`));
+    }
   }
 };
