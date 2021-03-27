48da9cc480b296c8fc378e97c249a6dae66bf3b9
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-03-27 20:02:58 +0300

    feat: main pug templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
+
+const PUBLIC_DIR = `public`;
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
