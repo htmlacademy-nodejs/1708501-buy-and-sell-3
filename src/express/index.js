f0c161f97e28b5b32ca6e33cc40b01c9b43abb1f
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-03-27 21:56:13 +0300

    feat: add basic templates

--- src/express/index.js
+++ src/express/index.js
@@ -3,19 +3,18 @@
 const path = require(`path`);
 const express = require(`express`);
 
-const PUBLIC_DIR = `public`;
-
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-app.set(`views`, path.resolve(__dirname, `templates`));
-app.set(`view engine`, `pug`);
-
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
