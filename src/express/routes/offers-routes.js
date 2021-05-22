5354cfa1f2bc23e996554dea8367373d093a4866
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-05-22 23:14:02 +0300

    feat: оживил редактирование тикета и поиск

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -2,11 +2,22 @@
 
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
+
 offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offers/ticket-edit`, {offer, categories});
+});
+
 offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
 
 module.exports = offersRouter;
