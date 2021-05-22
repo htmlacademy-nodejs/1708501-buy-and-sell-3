c487553f6c29f625f3820dc39564d4bcd19f82f2
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-05-22 22:43:11 +0300

    feat: добавил обработку my путей

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const items = await api.getOffers();
+  res.render(`main`, {items});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,9 +2,17 @@
 
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
+myRouter.get(`/`, async (req, res) => {
+  const items = await api.getOffers();
+  res.render(`my-tickets`, {items});
+});
+
+myRouter.get(`/comments`, async (req, res) => {
+  const items = await api.getOffers();
+  res.render(`comments`, {items: items.slice(0, 3)});
+});
 module.exports = myRouter;
